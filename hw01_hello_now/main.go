babe3106cbfbfe9b8a288ee97b6ae32afa5e0e49
Artem <shtikovaa@gmail.com>
2020-03-23 20:48:15 +0300

    Home work 1 is completed.

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	time, err := ntp.Time("2.pool.ntp.org")
+
+	if err != nil {
+		fmt.Println("Error -> main: ", err)
+		os.Exit(1)
+	}
+
+	curhour, curmin, cursec := time.Clock()
+	fmt.Printf("Current time: [%d:%d:%d]\n", curhour, curmin, cursec)
+
+	os.Exit(0)
 }
